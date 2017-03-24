c52ccd1470f8984c1e871345f435c90ae7c902e8
Eric Dufresne <e-dufresne@hotmail.com>
2017-03-24 19:41:18 -0400

    Added Angular-2

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Csi2132projectPage } from './app.po';
+
+describe('csi2132project App', function() {
+  let page: Csi2132projectPage;
+
+  beforeEach(() => {
+    page = new Csi2132projectPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
