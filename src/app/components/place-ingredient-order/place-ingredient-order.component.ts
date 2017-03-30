fc1c97c4d10393f53df9316661cdf0ecfd4a2268
Adam <adam.pglavine@gmail.com>
2017-03-30 18:07:01 -0400

    Edited HTML and TS files for place-ingredient-order admin view.
    TODO: must list ALL ingredients (currently unavailable ones are NOT present)
    Then, must allow users to click 'add' up to the ingredient threshold (ingredient threshold - currentCount = number of clicks possible?)
    Once admin hits 'CHECKOUT' button, ingredients should be added back into Database

--- src/app/components/place-ingredient-order/place-ingredient-order.component.ts
+++ src/app/components/place-ingredient-order/place-ingredient-order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FoodService} from "../../services/food.service";
 import { Router } from '@angular/router';
-
+import { ActivatedRoute } from '@angular/router';
+import {router} from "../../app.router";
 
 @Component({
   selector: 'app-place-ingredient-order',
@@ -8,15 +10,105 @@ import { Router } from '@angular/router';
   styleUrls: ['./place-ingredient-order.component.css']
 })
 export class PlaceIngredientOrderComponent implements OnInit {
-
-  constructor(public parentRouter : Router) {
+  public ingredients : any = [];
+  public searchString : string = '';
+  public itemsPerPage : number = 5;
+  public currentPage : number = 1;
+  constructor(public placeOrder : FoodService, public parentRouter : Router, public activatedRoute : ActivatedRoute) {
+    let successHandler = (data) => {
+      this.ingredients = data;
+      for (let ingredient of this.ingredients){
+        ingredient.selected = false;
+        ingredient.count = 0;
+      }
+    };
+    let errorHandler = (err) => {
+      console.error(err);
+    };
+    this.placeOrder.listFood().subscribe(successHandler, errorHandler);
   }
-
+  public selectIngredient(ingredient: any){
+    let index = this.ingredients.indexOf(ingredient);
+    if (index < 0){
+      return;
+    }
+    this.ingredients[index].selected = !this.ingredients[index].selected;
+  }
   ngOnInit() {
   }
-  logout(){
-    this.parentRouter.navigateByUrl('/login').catch(err => {
-      console.error(err);
-    })
+  public selectItem(ingredient : any){
+    ingredient.selected = true;
+    ingredient.count = 1;
+    console.log(ingredient.count);
   }
+  public deselectItem(ingredient: any){
+    ingredient.selected = false;
+    ingredient.count = 0;
+  }
+  public incrementItem(ingredient: any){
+    if (ingredient.count === ingredient.num_of_items){
+      return;
+    }
+    ingredient.count++;
+  }
+  public decrementItem(ingredient: any){
+    if (ingredient.count <= 1){
+      return;
+    }
+    ingredient.count--;
+  }
+  public totalPageCount(){
+    let val = Math.ceil(this.getUnselectedIngredients().length/this.itemsPerPage);
+    let arr = [];
+    for (let i = 1;i<=val;i++){
+      arr.push(i);
+    }
+    return arr;
+  }
+  public getPaginatedResults(){
+    let unselectedIngredients = this.getUnselectedIngredients();
+    let first = (this.currentPage-1) * this.itemsPerPage;
+    let last = (this.currentPage * this.itemsPerPage);
+
+    return unselectedIngredients.slice(first, last);
+  }
+  public getUnselectedIngredients(){
+    let modifiedSearchString = this.searchString.trim().toLowerCase();
+    return this.ingredients.filter((item) => {
+      if (modifiedSearchString.length == 0){
+        return item.selected === false;
+      }
+      else{
+        return item.selected === false && item.name.trim().toLowerCase().includes(modifiedSearchString);
+      }
+    });
+  }
+  public getSelectedIngredients(){
+    return this.ingredients.filter((item) => {return item.selected === true});
+  }
+  public selectionCount(){
+    return this.getSelectedIngredients().length;
+  }
+  public getTotalPrice(){
+    let selectedItems = this.getSelectedIngredients();
+    let totalPrice = 0;
+    for (let ingredient of selectedItems){
+      totalPrice += ingredient.count * ingredient.price_per_item;
+    }
+    return totalPrice;
+  }
+
+
+  public checkout(){
+    let errorHandler = (err) => {
+      console.error(err);
+    };
+    let successHandler = (success) => {
+      this.parentRouter.navigateByUrl('/home-user/checkout-success').catch(err => {
+        console.error(err);
+      });
+    };
+    this.placeOrder.checkout(this.getSelectedIngredients()).subscribe(successHandler, errorHandler);
+  }
+
 }
